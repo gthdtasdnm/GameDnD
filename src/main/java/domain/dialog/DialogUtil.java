926d2fd0c9ce224a785855ac7180b1be55c993b9
Cengiz <ata.kaan.cengiz@outlook.de>
2025-07-30 10:43:30 +0200

    DialogRepository

--- src/main/java/domain/dialog/DialogUtil.java
+++
@@ -1,7 +0,0 @@
-package domain.dialog;
-
-public class DialogUtil {
-    public static DialogInstance createEndDialog() {
-        return new DialogInstance("SYSTEM", "ENDE", new Information());
-    }
-}
